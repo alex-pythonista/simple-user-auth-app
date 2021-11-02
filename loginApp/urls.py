5de4c8926f239f5d85157ad90838e1a4644287cc
alex-pythonista <indrajit.alex@gmail.com>
2021-11-02 15:05:54 +0600

    register template and view created

--- loginApp/urls.py
+++ loginApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'loginApp'
 
 urlpatterns = [
+    path('register/', views.register, name='register'),
     
 ]
--- loginApp/views.py
+++ loginApp/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+from django.contrib.auth.models import User
+from .models import UserProfile
+
 # Create your views here.
 
+def register(request):
+
+    context = {}
+    template_name = 'loginApp/register.html'
+    return render(request, template_name, context)
